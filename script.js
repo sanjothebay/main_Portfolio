edb2bf4ad47d06def3a2559502a74364ee400ed6
JC <sanjothebay@gmail.com>
2020-11-15 14:24:28 -0600

    adding functions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = ( toggleId, navId) => {
-    const toggle = $("#toggleId");
-    nav = $("navId")
-    
-    if (togge && nav) {
-        toggle.on("click", () =>{
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
     }
@@ -13,4 +13,18 @@ const showMenu = ( toggleId, navId) => {
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+/*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
